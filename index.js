49d0f1ffe89a57d2c2c5416cf7399a4149dbf518
GBKwame <77574041+GBKwame@users.noreply.github.com>
2026-06-21 21:15:48 -0600

    Update index.js

--- index.js
+++ index.js
@@ -22,5 +22,9 @@ app.use('/uploads',express.static(path.join(__dirname,'uploads')))
 app.use('/user-api',profileRouter)
 app.use('/admin-api',AdminRouter)
 
-app.listen(3000,()=>console.log('server is running on port 3000'))
+const PORT = process.env.PORT || 3000;
 
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
+})
